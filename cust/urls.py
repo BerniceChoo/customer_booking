c759cc70e7f7c10dbedbda24b084b765635eca31
c2-foonyee <choo2.foonyee@live.uwe.ac.uk>
2023-01-14 03:54:41 +0000

    edited code

--- cust/urls.py
+++ cust/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path("selectDate/", views.selectDate, name="selectDate"),
     path("selectDate/booking/<str:selectedDate>", views.booking, name="booking"),
     path("payment/<int:adultQuantity>-<int:studentQuantity>-<int:childQuantity>-<int:selectedShowing>", views.payment, name="payment"),
-
-    path("sampleData/", views.sampleData, name="sampleData"),
 ]
